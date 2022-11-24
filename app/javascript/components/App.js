a2a9ba002e62cd2a543273dbe0608aeeee311158
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-23 23:30:53 -0300

    Fix App.js syntax error

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Greeting from './Greeting';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
 
 const App = () => (
-  <Provider store={store}></Provider>
+  <Provider store={store}>
     <Greeting />
   </Provider>
 );
